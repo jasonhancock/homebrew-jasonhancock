9bb38df2b1c6d680dea63c268d100b42bf1a5e4d
goreleaserbot <jason@jasonhancock.com>
2025-01-28 14:14:19 -0800

    Brew formula update for jasongen version v0.0.18

--- Formula/jasongen.rb
+++ Formula/jasongen.rb
@@ -5,20 +5,20 @@
 class Jasongen < Formula
   desc "jasongen openapi code generator"
   homepage "https://github.com/jasonhancock/jasongen"
-  version "0.0.17"
+  version "0.0.18"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.17/jasongen_0.0.17_darwin_amd64.tar.gz"
-      sha256 "3c9c7baf1130e6b5be34fe5baa46f9641db08f32b94af31c5d3fcf228880acc4"
+      url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.18/jasongen_0.0.18_darwin_amd64.tar.gz"
+      sha256 "dc0c231075f488afcb80310fcad5debf51d8f13e8636220ce4cbecb86f320c36"
 
       def install
         bin.install "jasongen"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.17/jasongen_0.0.17_darwin_arm64.tar.gz"
-      sha256 "53525466cff015f7178671472d8d2b25f406d49d8f771f4f5d387f0f6fb988de"
+      url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.18/jasongen_0.0.18_darwin_arm64.tar.gz"
+      sha256 "11289514bc38e2eed906d59975ca6404863992d89a51c46ffd8f0a28816431b4"
 
       def install
         bin.install "jasongen"
@@ -29,8 +29,8 @@ class Jasongen < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.17/jasongen_0.0.17_linux_amd64.tar.gz"
-        sha256 "09ea84070f493882989934c97aee738f4d3b47e5bacaea69f03c2c69f3d36d13"
+        url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.18/jasongen_0.0.18_linux_amd64.tar.gz"
+        sha256 "b52993da24e0b140b593042d2dcc9917ce312792b96ceae7187271418e549660"
 
         def install
           bin.install "jasongen"
@@ -39,8 +39,8 @@ class Jasongen < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.17/jasongen_0.0.17_linux_arm64.tar.gz"
-        sha256 "c59778a9ea83b5339805f805c92e25255e1912ea59972ec8c4e997dcee022734"
+        url "https://github.com/jasonhancock/jasongen/releases/download/v0.0.18/jasongen_0.0.18_linux_arm64.tar.gz"
+        sha256 "613ac5a6b9bdc9b7badd25bb81a59bc3943926594a7dcbb6ffdac4a0a8714301"
 
         def install
           bin.install "jasongen"
